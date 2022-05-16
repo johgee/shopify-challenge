fa0b257ae49bdc44c23bc05f4a7cec4793eb2631
Grace Joh <graceeunaejoh@gmail.com>
2022-05-16 16:15:14 -0400

    added controller and routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,36 @@
 class ItemsController < ApplicationController
+  def index
+    items = Item.all
+    render json: items.as_json
+  end
+
+  def create
+    item = Item.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      image_url: params[:image_url],
+    )
+    item.save
+    render json: item.as_json
+  end
+
+  def show
+    item = Item.find_by(id: params[:id])
+    render json: item.as_json
+  end
+
+  def update
+    item = Item.find_by(id: params[:id])
+    item.name = params[:name] || item.name
+    item.price = params[:price] || item.price
+    item.description = params[:description] || item.description
+    item.image_url = params[:image_url] || item.image_url
+  end
+
+  def destroy
+    item = Item.find_by(id: params[:id])
+    item.destroy
+    render json: { message: "Item succesfully destroyed." }
+  end
 end

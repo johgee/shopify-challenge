b4e151cc5f16a16488c3d21f50f9606fc2a86098
Grace Joh <graceeunaejoh@gmail.com>
2022-05-16 14:08:35 -0400

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+item = Item.new({ name: "cetaphil", price: 9, description: "for people with dry skin", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlKyNXrbsSYyJ5-Ah_xthpuC_BXeP1kOGJGQ&usqp=CAU" })
+item.save
+
+item = Item.new({ name: "cerave", price: 11, description: "for people with really really dry skin", image_url: "https://images.heb.com/is/image/HEBGrocery/001281037?fit=constrain,1&wid=800&hei=800&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0" })
+item.save
+
+item = Item.new({ name: "chanel", price: 50, description: "for people who don't use lotion", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9OSROaC-6saMIsxiJ_z-ierTooT3gyGqNHg&usqp=CAU" })
+item.save
+
+item = Item.new({ name: "aveda", price: 25, description: "for people who like the smell", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-cvQKbVXONMWbjWD53OJh_a8jL22pFaSrAw&usqp=CAU" })
+item.save
+
+item = Item.new({ name: "byredo", price: 35, description: "for people who like perfume", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrYBqaWaT3ZO5ToTgJfLbTCLADclFQTwukzg&usqp=CAU" })
+item.save
+
+item = Item.new({ name: "kiehl", price: 25, description: "for people who don't want smell", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4OO3MHPqMmuJ7Slieo6t9srIxtUeA2MncXek-WT_u8uCn0fiROPPQQ04f65Cuf_xxw1E&usqp=CAU" })
+item.save
+
+item = Item.new({ name: "fresh", price: 24, description: "for people who want smell", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1V3FXHyinbTO5uMBDnn7WU9w4mKEdch1lYA&usqp=CAU" })
+item.save

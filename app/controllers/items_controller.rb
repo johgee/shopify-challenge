0995c8863947fe7259d6b338290b56110698402b
Grace Joh <graceeunaejoh@gmail.com>
2022-05-17 18:59:44 -0400

    added undelete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,11 +22,15 @@ class ItemsController < ApplicationController
 
   def update
     item = Item.with_deleted.find_by(id: params[:id])
-    item.name = params[:name] || item.name
-    item.price = params[:price] || item.price
-    item.description = params[:description] || item.description
-    item.image_url = params[:image_url] || item.image_url
-    item.deleted_at = params[:deleted_at] || item.deleted_at
+    if item.deleted_at && params[:deleted_at] == nil
+      item.restore
+    else
+      item.name = params[:name] || item.name
+      item.price = params[:price] || item.price
+      item.description = params[:description] || item.description
+      item.image_url = params[:image_url] || item.image_url
+    end
+    item.delete_comment = params.has_key?(:delete_comment) ? params[:delete_comment] : item.delete_comment
     item.save
     render json: item.as_json
   end
